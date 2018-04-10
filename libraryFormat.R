ce9b62ca994952775599c603a3679ba02566a419
znoor <zainab.noor@students.mq.edu.au>
2018-04-10 14:02:37 +1000

    Standardise a data frame into library format

---
+++ libraryFormat.R
@@ -0,0 +1,61 @@
+###########################################################################
+#' Standardise a data frame into library format
+#' @param datlib a data frame for a spectrum library
+#' @param colnums a number representing the number of columns to keep 
+#' (default as 18)
+#' @return a data frame with specified number of columns
+#' @examples 
+#' libfile <- paste(system.file("files",package="SwathXtend"),"Lib2.txt",sep="/")
+#' datlib <- readLibFile(libfile)
+#' dat <- libraryFormat(datlib)
+############################################################################ 
+
+libraryFormat <- function(datlib)
+{
+  
+  inclcols = c("Q1",
+			"Q3",
+			"RT_detected",
+			"protein_name",
+			"isotype",
+			"relative_intensity",
+			"stripped_sequence",
+			"modification_sequence",
+			"prec_z",
+			"frg_type",
+			"frg_z",
+			"frg_nr",
+			"iRT",
+			"uniprot_id",
+			"score",
+			"decoy",
+			"prec_y",
+			"confidence",
+			"shared",
+			"N",
+			"rank",
+			"mods",
+			"nterm",
+			"cterm")
+	
+	if( all(inclcols %in% colnames(datlib)) )	{	
+		res = datlib[,inclcols]
+	}
+	else {
+		miss.col = inclcols[!inclcols %in% colnames(datlib)]
+		
+		toadd.col = matrix(NA, ncol=length(miss.col), nrow=nrow(datlib))
+		colnames(toadd.col) = miss.col		
+		
+		res = cbind(datlib, toadd.col)
+		
+		# check if all columns are included
+		
+		stopifnot(all(inclcols %in% colnames(res)))
+		
+		# re-order
+		res = res[,inclcols]
+	
+	}
+	res
+}
