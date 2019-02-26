e03d69d079e7cc7b213c1eb090da32fb9dda4c80
znoor <zainab.noor@students.mq.edu.au>
2019-02-26 11:38:02 +1100

    Plot the statistical summary of the base and external libraries

--- plotStats.R
+++ plotStats.R
@@ -72,7 +72,7 @@ plotStats <- function(datBaseLib, datExtLib, wb=NULL, sheet=NULL, ...)
     
     #////////////////////////////////////////////////////////////////////////////////////////////////////////
     
-      ## Venn diagrams
+      ## VENN DIAGRAMS
 
     #grid.newpage()
 
@@ -81,19 +81,43 @@ plotStats <- function(datBaseLib, datExtLib, wb=NULL, sheet=NULL, ...)
 
       filepath <- getwd()
   filepath2 <- paste0(filepath,"/graphs/proteinVennDigram.png")
-  png(filepath2)
+  # png(filepath2)
       # png("proteinVennDigram.png")
 
+  prot_base <- datBaseLib[!duplicated(datBaseLib$uniprot_id), "uniprot_id"]
+  pep_base <- datBaseLib[!duplicated(datBaseLib$stripped_sequence), "stripped_sequence"]
+  
+  prot_ext <- datExtLib[!duplicated(datExtLib$uniprot_id), "uniprot_id"]
+  pep_ext <- datExtLib[!duplicated(datExtLib$stripped_sequence), "stripped_sequence"]
+  
+ venn.diagram(list(prot_base, prot_ext),
+               category.names = c("Base Library Proteins", "External Library Proteins"),
+               resolution = 500,
+               cat.default.pos = "outer",
+               fill = c("#fb1f77", "#0080ff"),
+               cat.pos = c(-170, 170),
+               alpha = c(0.5, 0.5), 
+               cex = 2,
+               cat.fontface = 2,
+               lty =2, 
+               # fontfamily ="sans",
+               filename = filepath2
+               )
 
-      draw.pairwise.venn(nums.pro[1], nums.pro[2],
-                         length(proteinOverlap(datBaseLib,datExtLib, ...)),
-                         category = c("Base Library Proteins", "External Library Proteins"),
-                         lty = rep("blank", 2),
-                         fill = c("light blue", "pink"),
-                         alpha = rep(0.5, 2), cat.pos = c(0,20),
-                         cat.dist = rep(0.025, 2))
+      # draw.pairwise.venn(nums.pro[1], nums.pro[2],
+      #                    length(proteinOverlap(datBaseLib,datExtLib, ...)),
+      #                    category = c("Base Library Proteins", "External Library Proteins"),
+      #                    lty = rep("blank", 2),
+      #                    fill = c("light blue", "pink"),
+      #                    alpha = rep(0.5, 2), cat.pos = c(0,20),
+      #                    cat.dist = rep(0.025, 2))
 
-      dev.off()
+ 
+ # library(grDevices)
+ # 
+ # png(file=filepath2)
+ # grid.draw(temp1)
+ #      dev.off()
 
     }
 
@@ -102,19 +126,35 @@ plotStats <- function(datBaseLib, datExtLib, wb=NULL, sheet=NULL, ...)
       
       filepath <- getwd()
       filepath2 <- paste0(filepath,"/graphs/peptideVennDigram.png")
-      png(filepath2)
+      # png(filepath2)
       # png("peptideVennDigram.png")
-      draw.pairwise.venn(nums.pep[1], nums.pep[2],
-                         length(unique(intersect(datBaseLib$stripped_sequence,
-                                                 datExtLib$stripped_sequence))),
-                         category = c("Base Library Peptides", "External Library Peptides"),
-                         lty = rep("blank", 2),
-                         fill = c("light blue", "pink"),
-                         alpha = rep(0.5, 2), cat.pos = c(0,20),
-                         cat.dist = rep(0.025, 2))
+      
+      venn.diagram(list(pep_base, pep_ext),
+                   category.names = c("Base Library Peptides", "External Library Peptides"),
+                   resolution = 500,
+                   cat.default.pos = "outer",
+                   fill = c("#fb1f77", "#0080ff"),
+                   cat.pos = c(-170, 170),
+                   alpha = c(0.5, 0.5), 
+                   cex = 2,
+                   cat.fontface = 2,
+                   lty =2, 
+                   # fontfamily ="sans",
+                   filename = filepath2
+      )
+      
+      # draw.pairwise.venn(nums.pep[1], nums.pep[2],
+      #                    length(unique(intersect(datBaseLib$stripped_sequence,
+      #                                            datExtLib$stripped_sequence))),
+      #                    category = c("Base Library Peptides", "External Library Peptides"),
+      #                    lty = rep("blank", 2),
+      #                    fill = c("light blue", "pink"),
+      #                    alpha = rep(0.5, 2), cat.pos = c(0,20),
+      #                    cat.dist = rep(0.025, 2))
 
-
-      dev.off()
+      # png(file=filepath2)
+      # grid.draw(temp2)
+      # dev.off()
       
       ####/////////////////////////////////////////////////////////////////////////////////
  
@@ -177,58 +217,90 @@ plotStats <- function(datBaseLib, datExtLib, wb=NULL, sheet=NULL, ...)
     
     # png("histogram_peptide_ion.png")
     
-    x<-aggregate(datBaseLib$uniprot_id, by=list(Protein=datBaseLib$uniprot_id, Peptide=datBaseLib$stripped_sequence), 
-                 FUN=function(x){length(x)})
-    y<-aggregate(x$Peptide, by=list(Protein=x$Protein), FUN=function(x){length(x)})
+    # x<-aggregate(datBaseLib$uniprot_id, by=list(Protein=datBaseLib$uniprot_id, Peptide=datBaseLib$stripped_sequence), 
+    #              FUN=function(x){length(x)})
+    # y<-aggregate(x$Peptide, by=list(Protein=x$Protein), FUN=function(x){length(x)})
     
+    datBaseLib2 <- datBaseLib[!duplicated(datBaseLib$modification_sequence),]
+    x <- aggregate(x = datBaseLib2$modification_sequence, by = list(Protein = datBaseLib2$uniprot_id, charge = datBaseLib2$prec_z),
+                   FUN = function(x) {length(x)})
+    x$charge <- as.character(x$charge)
+    breaks1 <- pretty(range(x$x), n = nclass.FD(x$x), min.n = 1)
+    bwidth1 <- breaks1[3] - breaks1[1]
     
-    p1 <- ggplot(data = x, aes(x = x)) +
-      geom_histogram(aes(fill = ..count..), binwidth = 5) +
-      ggtitle("Base lib") + labs(x ="Number of ions per peptide") +
-      scale_y_continuous(name = "Frequency") +
-      scale_fill_gradient(low = "blue", high = "red")+
+    x2 <- aggregate(x = datBaseLib$modification_sequence, by = list(Peptide = datBaseLib$modification_sequence, 
+                                                                  frg_type = datBaseLib$frg_type), FUN = function(x2) {length(x2)})
+    breaks2 <- pretty(range(x2$x), n = nclass.FD(x2$x), min.n = 1)
+    bwidth2 <- breaks2[2] - breaks2[1]
+    
+    p1 <- ggplot(data = x, aes(x = x, fill = charge)) +
+      geom_histogram(binwidth = bwidth1) +
+      ggtitle("Base lib") +
+      labs(x ="Number of Peptides", y = "Number of Proteins") +
+      # scale_y_continuous(name = "Frequency") +
+      scale_fill_brewer("Precursor Charge", palette = "YlOrRd") +
       theme_classic() +
       theme(plot.title = element_text(size = 12, face = "bold"),
             axis.title = element_text(size = 11, face = "bold"),
             axis.text.x = element_text(face = "bold", size = 10),
-            axis.text.y = element_text(face= "bold", size = 10)) 
+            axis.text.y = element_text(face= "bold", size = 10))  
     
-    p2 <- ggplot(data = y, aes(x = x)) +
-      geom_histogram(aes(fill = ..count..), binwidth = 5) +
-      ggtitle("Base lib") + labs(x ="Number of peptides per protein") +
-      scale_y_continuous(name = "Frequency") +
-      scale_fill_gradient(low = "blue", high = "red")+
+    p2 <- ggplot(data = x2, aes(x = x, fill = frg_type)) +
+      geom_histogram(binwidth = bwidth2) +
+      labs(x ="Number of Ions", y = "Number of Peptides") +
+      ggtitle("Base lib") +
+      scale_fill_brewer("Fragment Type", palette = "PuRd") +
+      # scale_fill_manual(values = c("#eeba30", "#ae0001")) +
+      # scale_y_continuous(name = "Frequency") +
+      # scale_fill_gradient("Frequency", low = "blue", high = "red")+
       theme_classic() +
       theme(plot.title = element_text(size = 12, face = "bold"),
             axis.title = element_text(size = 11, face = "bold"),
             axis.text.x = element_text(face = "bold", size = 10),
-            axis.text.y = element_text(face= "bold", size = 10)) 
+            axis.text.y = element_text(face= "bold", size = 10))  
     
-    x<-aggregate(datExtLib$uniprot_id, by=list(Protein=datExtLib$uniprot_id, Peptide=datExtLib$stripped_sequence), 
-                 FUN=function(x){length(x)})
-    y<-aggregate(x$Peptide, by=list(Protein=x$Protein), FUN=function(x){length(x)})
+    #################################################################################################
     
-    p3 <- ggplot(data = x, aes(x = x)) +
-      geom_histogram(aes(fill = ..count..), binwidth = 5) +
-      ggtitle("Addon lib") + labs(x ="Number of ions per peptide") +
-      scale_y_continuous(name = "Frequency") +
-      scale_fill_gradient(low = "blue", high = "red")+
+    # x<-aggregate(datExtLib$uniprot_id, by=list(Protein=datExtLib$uniprot_id, Peptide=datExtLib$stripped_sequence), 
+    #              FUN=function(x){length(x)})
+    # y<-aggregate(x$Peptide, by=list(Protein=x$Protein), FUN=function(x){length(x)})
+    
+    datExtLib2 <- datExtLib[!duplicated(datExtLib$modification_sequence),]
+    x <- aggregate(x = datExtLib2$modification_sequence, by = list(Protein = datExtLib2$uniprot_id, charge = datExtLib2$prec_z),
+                   FUN = function(x) {length(x)})
+    x$charge <- as.character(x$charge)
+    breaks1 <- pretty(range(x$x), n = nclass.FD(x$x), min.n = 1)
+    bwidth1 <- breaks1[3] - breaks1[1]
+    
+    x2 <- aggregate(x = datExtLib$modification_sequence, by = list(Peptide = datExtLib$modification_sequence, 
+                                                                  frg_type = datExtLib$frg_type), FUN = function(x2) {length(x2)})
+    breaks2 <- pretty(range(x2$x), n = nclass.FD(x2$x), min.n = 1)
+    bwidth2 <- breaks2[2] - breaks2[1]
+    
+    p3 <- ggplot(data = x, aes(x = x, fill = charge)) +
+      geom_histogram(binwidth = bwidth1) +
+      labs(x ="Number of Peptides", y = "Number of Proteins") +
+      ggtitle("Addon lib") +
+      # scale_y_continuous(name = "Frequency") +
+      scale_fill_brewer("Precursor Charge", palette = "YlOrRd") +
       theme_classic() +
       theme(plot.title = element_text(size = 12, face = "bold"),
             axis.title = element_text(size = 11, face = "bold"),
             axis.text.x = element_text(face = "bold", size = 10),
-            axis.text.y = element_text(face= "bold", size = 10)) 
+            axis.text.y = element_text(face= "bold", size = 10))  
     
-    p4 <- ggplot(data = y, aes(x = x)) +
-      geom_histogram(aes(fill = ..count..), binwidth = 5) +
-      ggtitle("Addon lib") + labs(x ="Number of peptides per protein") +
-      scale_y_continuous(name = "Frequency") +
-      scale_fill_gradient(low = "blue", high = "red")+
+    p4 <- ggplot(data = x2, aes(x = x, fill = frg_type)) +
+      geom_histogram(binwidth = bwidth2) +
+      labs(x ="Number of Ions", y = "Number of Peptides") +
+      ggtitle("Addon lib") +
+      scale_fill_brewer("Fragment type", palette = "PuRd") +
+      # scale_y_continuous(name = "Frequency") +
+      # scale_fill_gradient("", low = "blue", high = "red")+
       theme_classic() +
       theme(plot.title = element_text(size = 12, face = "bold"),
             axis.title = element_text(size = 11, face = "bold"),
             axis.text.x = element_text(face = "bold", size = 10),
-            axis.text.y = element_text(face= "bold", size = 10)) 
+            axis.text.y = element_text(face= "bold", size = 10))
     
     phist <- ggarrange(p1, p2, p3, p4, ncol = 2, nrow = 2)
     
